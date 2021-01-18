a2fb5170ba8e06b904bfec61ca794ed5ecd08595
Rakesh Agrawal <rakesh.agrawal1502@gmail.com>
2021-01-18 16:26:43 +0530

    Added network configuration in home module for inter container communication

--- home/controllers.py
+++ home/controllers.py
@@ -3,17 +3,13 @@ from flask import jsonify, request, render_template, Blueprint
 home = Blueprint( 'home', __name__ )
 
 
-@home.route( "/", methods=['GET'] )
+@home.route( "/home", methods=['GET'] )
 def land():
     # return "<h1>Hello World</h1>"
     return render_template( "home.html" )
 
 
-@home.route( "/about", methods=['GET'] )
+@home.route( "/home/about", methods=['GET'] )
 def about():
     return render_template( "about.html" )
 
-
-@home.route( '/<name>' )
-def hello_name(name):
-    return "Hello " + name
