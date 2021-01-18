3fa6c8e5f93812f6ebc9d5792534e88790a01665
Rakesh Agrawal <rakesh.agrawal1502@gmail.com>
2021-01-18 09:45:43 +0530

    DB Microservice

---
+++ database/init.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA IF NOT EXISTS sample;
+
+CREATE SEQUENCE sample.customers_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE SEQUENCE sample.invoices_invoice_number_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE TABLE sample.customers
+(
+    id integer,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    address character varying COLLATE pg_catalog."default",
+    email character varying COLLATE pg_catalog."default" NOT NULL,
+    created_on timestamp without time zone,
+    updated_on timestamp without time zone,
+    CONSTRAINT customers_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE sample.invoices
+(
+    invoice_number integer,
+    customer_id integer,
+    amount integer,
+    CONSTRAINT invoices_pkey PRIMARY KEY (invoice_number),
+    CONSTRAINT invoices_customer_id_fkey FOREIGN KEY (customer_id)
+        REFERENCES sample.customers (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
