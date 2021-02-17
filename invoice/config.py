e227462bb1e8277d7d69a4a2182cbf263dff166e
Rakesh Agrawal <rakesh.agrawal1502@gmail.com>
2021-02-17 15:36:14 +0530

    Changes for having invoice module as kubernetes cluster

--- invoice/config.py
+++ invoice/config.py
@@ -1,6 +1,11 @@
 # Define the application directory
 import os
 
+POSTGRES_USER = os.environ.get( 'POSTGRES_USER' )
+POSTGRES_PASSWORD = os.environ.get( 'POSTGRES_PASSWORD' )
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+DB_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/flask'
+
 
 class Config( object ):
     BASE_DIR = os.path.abspath( os.path.dirname( __file__ ) )
@@ -8,7 +13,9 @@ class Config( object ):
     DEBUG = False
     # DATABASE_URI = "postgresql+psycopg2://postgres:password@localhost:5432/flask"
     # DATABASE_URI = "postgresql+psycopg2://postgres:password@localhost:5434/flask"
-    DATABASE_URI = "postgresql+psycopg2://postgres:password@db:5432/flask"
+    # DATABASE_URI = "postgresql+psycopg2://postgres:password@db:5432/flask"
+    # DATABASE_URI = "postgresql+psycopg2://postgres:password@localhost:5434/flask"
+    DATABASE_URI = DB_URL
 
 
 class DevelopmentConfig( Config ):
